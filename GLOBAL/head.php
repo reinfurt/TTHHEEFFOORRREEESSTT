d05207c631bafe7706f13b89726cdc6b907a0972
David Reinfurt <reinfurt@o-r-g.com>
2014-07-13 12:51:15 -0400

    Add expandable images in cloud with random widths

--- GLOBAL/head.php
+++ GLOBAL/head.php
@@ -1,4 +1,5 @@
 <?php 
+        date_default_timezone_set('America/New_York');
 	require_once("_Library/systemDatabase.php"); 
 	require_once("_Library/systemCookie.php");
 	require_once("_Library/displayNavigation.php"); 
